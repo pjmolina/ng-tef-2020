98a658770c790ec2beb36fec7b2bf152365bf45d
Pedro J. Molina <pjmolina@gmail.com>
2020-10-26 10:28:35 +0100

    fix tests in app injecting the pipe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SearchByPipe } from './pipes/search-by.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SearchByPipe
       ],
     }).compileComponents();
   });
